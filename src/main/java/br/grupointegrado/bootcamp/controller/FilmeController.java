bfc0b5a502d08b0891f1e4789e6414e10f5771d1
Ivan Ferreira <ivan.marcelo.ferreira@gmail.com>
2024-10-24 20:02:35 -0300

    chore: adding findByI

--- src/main/java/br/grupointegrado/bootcamp/controller/FilmeController.java
+++ src/main/java/br/grupointegrado/bootcamp/controller/FilmeController.java
@@ -4,19 +4,33 @@ import br.grupointegrado.bootcamp.model.Filme;
 import br.grupointegrado.bootcamp.repository.FilmeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/filmes")
 public class FilmeController {
 
     @Autowired
     private FilmeRepository repository;
 
-    @GetMapping("/filmes")
+    @GetMapping
     public List<Filme> findAll() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Filme findById(@PathVariable Integer id) {
+        return this.repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Produto não encontrado"));
+    }
+
+    @GetMapping("/nome/{nome}")
+    public List<Filme> findById(@PathVariable String nome) {
+        return this.repository.findByNomeLike(nome);
     }
 
 }
--- src/main/java/br/grupointegrado/bootcamp/repository/FilmeRepository.java
+++ src/main/java/br/grupointegrado/bootcamp/repository/FilmeRepository.java
@@ -3,5 +3,9 @@ package br.grupointegrado.bootcamp.repository;
 import br.grupointegrado.bootcamp.model.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FilmeRepository extends JpaRepository<Filme, Integer> {
+
+    List<Filme> findByNomeLike(String nome);
 }
